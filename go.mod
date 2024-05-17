7b0706bf35ad077b14fbb722ad6835d030d33448
Николай Притыкин <nikolajpritykin@MacBook-Air-Nikolaj.local>
2024-05-17 22:59:18 +0700

    postgres database funcs to restapi app

--- go.mod
+++ go.mod
@@ -1,4 +1,4 @@
-module github.com/NikolasDeForce/restdb
+module restdb
 
 go 1.21.6
 
--- test_db/db_test.go
+++ test_db/db_test.go
@@ -7,7 +7,7 @@ import (
 	//"testing"
 	"time"
 
-	"github.com/NikolasDeForce/restdb"
+	"restdb"
 )
 
 type User struct {
